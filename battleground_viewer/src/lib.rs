8e686a7ae98d9db59312308aee92e13aea198f3c
Ivor Wanders <ivor@iwanders.net>
2022-11-18 12:24:40 -0500

    Expiriment with shadow.

--- battleground_viewer/src/lib.rs
+++ battleground_viewer/src/lib.rs
@@ -64,16 +64,36 @@ pub fn main() {
     sphere.set_transformation(Mat4::from_translation(vec3(0.0, 0.0, 0.0)) * Mat4::from_scale(0.2));
 
 
+    let mut cpu_plane = CpuMesh::square();
+    let mut ground_plane = Gm::new(
+        Mesh::new(&context, &cpu_plane),
+        PhysicalMaterial::new_opaque(
+            &context,
+            &CpuMaterial {
+                albedo: Color::new_opaque(128, 128, 128),
+                ..Default::default()
+            },
+        ),
+    );
+    ground_plane.set_transformation(Mat4::from_translation(vec3(0.0, 0.0, 0.0)) * Mat4::from_scale(1000.0) * Mat4::from_angle_x(Rad(std::f32::consts::PI / 2.0)));
+
+    let mut deferred_plane = Gm::new(
+        Mesh::new(&context, &cpu_plane),
+        DeferredPhysicalMaterial::from_physical_material(&ground_plane.material),
+    );
+    deferred_plane.set_transformation(Mat4::from_translation(vec3(0.0, 0.0, 0.0)) * Mat4::from_scale(1000.0) * Mat4::from_angle_x(Rad(std::f32::consts::PI / 2.0)));
+
+
     let mut cube = Gm::new(
         Mesh::new(&context, &CpuMesh::cube()),
-        PhysicalMaterial::new_transparent(
+        three_d::renderer::material::PhysicalMaterial::new_opaque(
             &context,
             &CpuMaterial {
                 albedo: Color {
                     r: 0,
                     g: 0,
                     b: 255,
-                    a: 100,
+                    a: 255,
                 },
                 ..Default::default()
             },
@@ -83,8 +103,8 @@ pub fn main() {
 
 
 
-    let light0 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, -0.5, -0.5));
-    let light1 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, 0.5, 0.5));
+    let light0 = three_d::renderer::light::AmbientLight::new(&context, 0.3, Color::WHITE);
+    let mut light1 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, -5.5, 0.5));
 
 
     let mut limiter = Limiter::new(0.01);
@@ -96,20 +116,24 @@ pub fn main() {
 
         // Set the cube's position... 
         let vehicle_id = construct.vehicle_id();
-        let pose = construct.world().component::<components::pose::Pose>(&vehicle_id).expect("Should have a pose for the vehicle");
+        let mut pose = *construct.world().component::<components::pose::Pose>(&vehicle_id).expect("Should have a pose for the vehicle");
+        pose.h.w[1] += 2.0;
         cube.set_transformation(pose.h);
 
         camera.set_viewport(frame_input.viewport);
         control.handle_events(&mut camera, &mut frame_input.events);
+
+        light1.generate_shadow_map(1024, &cube);
 
         frame_input
             .screen()
             .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
             .render(
                 &camera,
-                sphere
+                deferred_plane
                     .into_iter()
-                    .chain(&cube),
+                    .chain(&cube)
+                    .chain(&sphere),
                 &[&light0, &light1],
             );
 
