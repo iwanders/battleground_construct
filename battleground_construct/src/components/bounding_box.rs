f97439ed05a43c8e41c3b8faf4adf8ad445e16b9
Ivor Wanders <ivor@iwanders.net>
2022-11-18 13:57:56 -0500

    Use the size of the bounding box.

--- battleground_construct/src/components/bounding_box.rs
+++ battleground_construct/src/components/bounding_box.rs
@@ -1,5 +1,6 @@
 use engine::prelude::*;
 
+#[derive(Copy, Debug, Clone)]
 pub struct BoundingBox {
     pub min: cgmath::Vector3<f32>,
     pub max: cgmath::Vector3<f32>,
@@ -11,6 +12,10 @@ impl BoundingBox {
             min: cgmath::Vector3::new(-0.5, -0.5, -0.5),
             max: cgmath::Vector3::new(0.5, 0.5, 0.5),
         }
+    }
+
+    pub fn dimensions(&self) -> cgmath::Vector3<f32> {
+        self.max - self.min
     }
 }
 impl Component for BoundingBox {}
