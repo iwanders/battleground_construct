90d510b3ac86a07460fc99a07af049fcb096b35c
Ivor Wanders <ivor@iwanders.net>
2022-11-18 10:37:35 -0500

    Add kinematic differential drive.

---
+++ battleground_construct/src/components/differential_drive_base.rs
@@ -0,0 +1,37 @@
+use engine::prelude::*;
+
+pub struct DifferentialDriveBase {
+    pub track_width: f32,
+    pub wheel_velocity_bounds: (f32, f32),
+    pub wheel_velocity: (f32, f32),
+}
+
+impl DifferentialDriveBase {
+    pub fn new() -> Self {
+        DifferentialDriveBase {
+            track_width: 1.0,
+            wheel_velocity_bounds: (-1.0, 1.0),
+            wheel_velocity: (0.2, 0.8),
+        }
+    }
+
+    pub fn set_velocities(&mut self, left: f32, right: f32) {
+        self.wheel_velocity = (
+            left.clamp(self.wheel_velocity_bounds.0, self.wheel_velocity_bounds.1),
+            right.clamp(self.wheel_velocity_bounds.0, self.wheel_velocity_bounds.1),
+        );
+    }
+
+    pub fn track_width(&self) -> f32 {
+        self.track_width
+    }
+
+    pub fn wheel_velocities(&self) -> (f32, f32) {
+        self.wheel_velocity
+    }
+
+    pub fn wheel_velocity_bounds(&mut self) -> (f32, f32) {
+        self.wheel_velocity_bounds
+    }
+}
+impl Component for DifferentialDriveBase {}
---
+++ battleground_construct/src/systems/kinematics_differential_drive.rs
@@ -0,0 +1,21 @@
+use super::components::differential_drive_base::DifferentialDriveBase;
+use super::components::velocity::Velocity;
+
+use engine::prelude::*;
+
+pub struct KinematicsDifferentialDrive {}
+impl System for KinematicsDifferentialDrive {
+    fn update(&mut self, world: &mut World) {
+        for (entity, base) in world.component_iter::<DifferentialDriveBase>() {
+            // try to see if we can find a velocity for this entity.
+            if let Some(mut vel) = world.component_mut::<Velocity>(&entity) {
+                // Yes, so set the velocity.
+                let wheel_velocities = base.wheel_velocities();
+                let track_width = base.track_width();
+                let linear_velocity = (wheel_velocities.0 + wheel_velocities.1) / 2.0;
+                let angular_velocity = (wheel_velocities.0 - wheel_velocities.1) / track_width;
+                *vel = Velocity::from_se2(linear_velocity, 0.0, angular_velocity);
+            }
+        }
+    }
+}
