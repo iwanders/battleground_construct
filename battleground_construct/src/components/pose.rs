eb1c8d10e9b6e94749ab382457d630da7f37cd1b
Ivor Wanders <ivor@iwanders.net>
2022-11-19 16:14:47 -0500

    Add a PreTransform.

--- battleground_construct/src/components/pose.rs
+++ battleground_construct/src/components/pose.rs
@@ -5,64 +5,63 @@ pub struct Pose {
     pub h: cgmath::Matrix4<f32>,
 }
 
-impl Pose {
-    pub fn new() -> Self {
-        Pose {
-            h: cgmath::Matrix4::<f32>::from_translation(cgmath::Vector3::new(0.0, 0.0, 0.0)),
-        }
-    }
-}
-impl Component for Pose {}
-
-impl std::ops::Deref for Pose {
-    type Target = cgmath::Matrix4<f32>;
-    fn deref(&self) -> &<Self as std::ops::Deref>::Target {
-        &self.h
-    }
-}
-
-impl From<cgmath::Matrix4<f32>> for Pose {
-    fn from(v: cgmath::Matrix4<f32>) -> Self {
-        Pose { h: v }
-    }
-}
-
-impl Into<cgmath::Matrix4<f32>> for Pose {
-    fn into(self) -> cgmath::Matrix4<f32> {
-        self.h
-    }
-}
-
-impl std::ops::Mul<Pose> for Pose {
-    type Output = Pose;
-    fn mul(self, v: Pose) -> <Self as std::ops::Mul<Pose>>::Output {
-        Pose{h: self.h * v.h}
-    }
-}
-
-/// Transform to go before the Pose operation, from world coordinates to entity coordinates.
 #[derive(Copy, Debug, Clone)]
-pub struct PreTransform(Pose);
-impl std::ops::Deref for PreTransform {
-    type Target = Pose;
-    fn deref(&self) -> &<Self as std::ops::Deref>::Target {
-        &self.0
-    }
+pub struct PreTransform {
+    pub h: cgmath::Matrix4<f32>,
 }
 
-impl std::ops::DerefMut for PreTransform {
-    fn deref_mut(&mut self) -> &mut <Self as std::ops::Deref>::Target {
-        &mut self.0
-    }
+
+macro_rules! create_transform_component {
+    ($the_type:ty) => {
+
+        impl $the_type {
+            pub fn new() -> Self {
+                Self {
+                    h: cgmath::Matrix4::<f32>::from_translation(cgmath::Vector3::new(0.0, 0.0, 0.0)),
+                }
+            }
+            pub fn from_translation(v: cgmath::Vector3<f32>) -> Self {
+                Self::from_mat4(cgmath::Matrix4::<f32>::from_translation(v))
+            }
+            pub fn from_mat4(h: cgmath::Matrix4::<f32>) -> Self {
+                Self {
+                    h
+                }
+            }
+            pub fn transform(&self) -> &cgmath::Matrix4::<f32> {
+                &self.h
+            }
+        }
+        impl Component for $the_type {}
+
+
+        impl std::ops::Deref for $the_type {
+            type Target = cgmath::Matrix4<f32>;
+            fn deref(&self) -> &<Self as std::ops::Deref>::Target {
+                &self.h
+            }
+        }
+
+        impl From<cgmath::Matrix4<f32>> for $the_type {
+            fn from(v: cgmath::Matrix4<f32>) -> Self {
+                <$the_type>::from_mat4(v)
+            }
+        }
+
+        impl Into<cgmath::Matrix4<f32>> for $the_type {
+            fn into(self) -> cgmath::Matrix4<f32> {
+                self.h
+            }
+        }
+
+        impl std::ops::Mul<$the_type> for $the_type {
+            type Output = $the_type;
+            fn mul(self, v: $the_type) -> <Self as std::ops::Mul<$the_type>>::Output {
+                <$the_type>::from_mat4(self.h * v.h)
+            }
+        }
+
+    };
 }
-
-impl PreTransform {
-    pub fn new() -> Self {
-        PreTransform (
-            Pose::new()
-        )
-    }
-}
-impl Component for PreTransform {}
-
-
+create_transform_component!(Pose);
+create_transform_component!(PreTransform);
