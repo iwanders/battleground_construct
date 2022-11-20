20872abd411b006d1a8fcbf38655a2608ea39a41
Ivor Wanders <ivor@iwanders.net>
2022-11-20 14:01:42 -0500

    Remove projectiles below floor.

--- battleground_construct/src/systems/mod.rs
+++ battleground_construct/src/systems/mod.rs
@@ -3,6 +3,7 @@ pub mod cannon_trigger;
 pub mod kinematics_differential_drive;
 pub mod revolute_pose;
 pub mod velocity_pose;
+pub mod projectile_floor;
 
 use super::components;
 use super::components::clock::Clock;
---
+++ battleground_construct/src/systems/projectile_floor.rs
@@ -0,0 +1,23 @@
+use super::components::pose::Pose;
+use super::components::point_projectile::PointProjectile;
+use super::Clock;
+use engine::prelude::*;
+
+pub struct ProjectileFloor {}
+impl System for ProjectileFloor {
+    fn update(&mut self, world: &mut World) {
+        for entity in world.component_entities::<PointProjectile>() {
+            let below_floor = {
+                if let Some(pose) = world.component_mut::<Pose>(&entity) {
+                    // Yes, so now integrate it.
+                    pose.transform().w[2] <= 0.0
+                } else {
+                    false
+                }
+            };
+            if below_floor {
+                world.remove_entity(&entity);
+            }
+        }
+    }
+}
