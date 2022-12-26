5fcae9aca0da1fc591ce986d06f1f8a25f8a1cbb
Ivor Wanders <ivor@iwanders.net>
2022-12-26 14:54:32 -0500

    Eliminate transparency from control point.

--- battleground_construct/src/display/display_control_point.rs
+++ battleground_construct/src/display/display_control_point.rs
@@ -20,13 +20,17 @@ impl DisplayControlPoint {
                 r: 128,
                 g: 128,
                 b: 128,
-                a: 128,
+                a: 255,
             },
         }
     }
     pub fn set_color(&mut self, color: Color) {
-        self.color = color;
-        self.color.a = 128;
+        // self.color = color;
+        // self.color.a = 128;
+        self.color.r = ((color.r as u32 + 128) / 2) as u8;
+        self.color.g = ((color.g as u32 + 128) / 2) as u8;
+        self.color.b = ((color.b as u32 + 128) / 2) as u8;
+        // self.color.a = ((color.a as u32 + 128) / 2) as u8;
     }
 
     pub fn set_radius(&mut self, radius: f32) {
@@ -39,11 +43,11 @@ impl Drawable for DisplayControlPoint {
     fn drawables(&self) -> Vec<Element> {
         let material = Material::FlatMaterial(FlatMaterial {
             color: self.color,
-            is_transparent: true,
+            is_transparent: false,
             ..Default::default()
         });
         vec![Element {
-            transform: Mat4::from_translation(Vec3::new(0.0, 0.0, 0.01)),
+            transform: Mat4::from_translation(Vec3::new(0.0, 0.0, 0.001)),
             primitive: Primitive::Circle(Circle {
                 radius: self.radius,
                 subdivisions: 30,
