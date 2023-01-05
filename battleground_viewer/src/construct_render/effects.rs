7f5d185397a226574a948e7462640acdc48b1eab
Brend Wanders <brend@13w.nl>
2023-01-05 16:14:51 +0100

    Rename methods to sane names.

--- battleground_viewer/src/construct_render/effects.rs
+++ battleground_viewer/src/construct_render/effects.rs
@@ -1,6 +1,6 @@
 use crate::construct_render::instanced_entity::InstancedEntity;
-use crate::construct_render::util::ColorConvert;
 use crate::construct_render::render::{RenderPass, RenderableGeometry};
+use crate::construct_render::util::ColorConvert;
 use battleground_construct::display;
 use battleground_construct::util::cgmath::prelude::*;
 use rand::rngs::ThreadRng;
@@ -347,7 +347,6 @@ impl RenderableGeometry for ParticleEmitter {
 
     fn finish_frame(&mut self, context: &Context) {}
 }
-
 
 // use battleground_construct::util::cgmath::InvertHomogeneous;
 use battleground_construct::util::cgmath::ToHomogenous;
